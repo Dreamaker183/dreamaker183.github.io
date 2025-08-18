11f8e4d45a46732aea1b49686f2c2cc2829d6c3f
Logan Collins <logansi5awkrdbq6hz30@gmail.com>
2025-08-18 07:26:44 +0000

    it also can automatic scroll

--- src/components/sections/tech-stack-section.tsx
+++ src/components/sections/tech-stack-section.tsx
@@ -23,7 +23,7 @@ export function TechStackSection() {
         <div
           className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]"
         >
-          <ul className="flex items-center justify-center md:justify-start [&_li]:mx-4 [&_img]:max-w-none animate-infinite-scroll">
+          <ul className="flex items-center justify-center md:justify-start [&_li]:mx-4 animate-infinite-scroll">
             {skills.map((skill) => (
               <li key={skill.name}>
                 <Tooltip>
@@ -39,7 +39,7 @@ export function TechStackSection() {
               </li>
             ))}
           </ul>
-           <ul className="flex items-center justify-center md:justify-start [&_li]:mx-4 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
+           <ul className="flex items-center justify-center md:justify-start [&_li]:mx-4 animate-infinite-scroll" aria-hidden="true">
             {skills.map((skill) => (
               <li key={skill.name}>
                 <Tooltip>

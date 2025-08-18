77ec981359a62c401c4abf720959060d98897e5c
Logan Collins <logansi5awkrdbq6hz30@gmail.com>
2025-08-18 07:37:22 +0000

    I mean it will scroll horizontal infinitly automatically

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,21 +78,15 @@ export default {
             height: "0",
           },
         },
-        "fly-in": {
-          from: {
-            opacity: "0",
-            transform: "translateY(2rem)",
-          },
-          to: {
-            opacity: "1",
-            transform: "translateY(0)",
-          },
-        },
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "fly-in": "fly-in 0.7s ease-out forwards",
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
       },
     },
   },
